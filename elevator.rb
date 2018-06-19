7c3ffa6ce7dea92b265f50580eef38208be5a756
Charlélie Boussaud <charlelie.boussaud@hotmail.fr>
2018-06-19 14:49:24 +0200

    add queue system, not done yet

--- elevator.rb
+++ elevator.rb
@@ -1,40 +1,64 @@
 class Elevator
 	
 	@@person = []
+	@@elevator = {status: "down", floor: 0}
+			
+	def self.sortQueue
+		tab_up, tab_down = @@person.partition { |x| x[:action] == "up" }
+	#	up_a, up_b = tab_up.partition { |x| x[:in_floor] > @@elevator[:floor] }
+	#	up_a.sort_by! { |k| k[:in_floor] }
+	#	tab_up = up_a.push(*up_b)
+	#	down_a, down_b = tab_down.partition { |x| x[:in_floor] > @@elevator[:floor] }
+	#	down_a.sort_by! { |k| k[:in_floor] }.reverse!
+	#	down_b.sort_by! { |k| k[:in_floor] }.reverse!
+	#   tab_down = down_b.push(*down_a)
+		tab_down.sort_by! { |k| k[:in_floor] }.reverse!
+		tab_up.sort_by! { |k| k[:in_floor] }
+		if @@elevator[:status] == "up"
+			@@person = tab_up.push(*tab_down)
+		elsif @@elevator[:status] == "down"
+			@@person = tab_down.push(*tab_up)
+		end
+	end
+
 	def protoElevator
-		elevator = {status: "up", floor: 0}
 		while true do
 			if @@person.any?
-				puts @@person
-				if @@person[0][:state] == 'off' and elevator[:floor] < @@person[0][:in_floor]
-					elevator[:floor] += 1
-				elsif @@person[0][:state] == 'off' and elevator[:floor] > @@person[0][:in_floor]
-					elevator[:floor] -= 1
-				elsif @@person[0][:state] == 'off' and elevator[:floor] == @@person[0][:in_floor]
+				if @@person[0][:state] == 'off' and @@elevator[:floor] < @@person[0][:in_floor]
+					@@elevator[:status] = "up"
+					@@elevator[:floor] += 1
+				elsif @@person[0][:state] == 'off' and @@elevator[:floor] > @@person[0][:in_floor]
+					@@elevator[:status] = "down"
+					@@elevator[:floor] -= 1
+				elsif @@person[0][:state] == 'off' and @@elevator[:floor] == @@person[0][:in_floor]
 					@@person[0][:state] = 'on'
 					if @@person[0][:action] == 'up'
-						if elevator[:floor] + 1 == 5
-							@@person[0][:goes_to] = 5
-						elsif
-							@@person[0][:goes_to] = rand(elevator[:floor] + 1...5)
+						if @@elevator[:floor] + 1 >= 5
+							@@person[0][:in_floor] = 5
+						else
+							@@person[0][:in_floor] = rand(@@elevator[:floor] + 1...5)
 						end
 					else
-						if elevator[:floor] - 1 == 0
-							@@person[0][:goes_to] = 0
-						elsif
-							@@person[0][:goes_to] = rand(0...elevator[:floor] - 1)
+						if @@elevator[:floor] - 1 <= 0
+							@@person[0][:in_floor] = 0
+						else
+							@@person[0][:in_floor] = rand(0...@@elevator[:floor] - 1)
 						end
 					end
-				elsif @@person[0][:state] == 'on' and elevator[:floor] < @@person[0][:goes_to]
-					elevator[:floor] += 1
-				elsif @@person[0][:state] == 'on' and elevator[:floor] > @@person[0][:goes_to]
-					elevator[:floor] -= 1
-				elsif @@person[0][:state] == 'on' and elevator[:floor] == @@person[0][:goes_to]
-					puts "shift elem"
+					puts "Person " + @@person[0][:id].to_s + " is entering the elevator and wants to go at floor " + @@person[0][:in_floor].to_s
+				elsif @@person[0][:state] == 'on' and @@elevator[:floor] < @@person[0][:in_floor]
+					@@elevator[:status] = "up"
+					@@elevator[:floor] += 1
+				elsif @@person[0][:state] == 'on' and @@elevator[:floor] > @@person[0][:in_floor]
+					@@elevator[:status] = "down"
+					@@elevator[:floor] -= 1
+				elsif @@person[0][:state] == 'on' and @@elevator[:floor] == @@person[0][:in_floor]
+					puts "Person " + @@person[0][:id].to_s + " is exiting the elevator"
 					@@person.shift
 				end
 			end
-			puts "Elevator is in floor: " + elevator[:floor].to_s
+			self.class.sortQueue
+			puts "Elevator is in floor: " + @@elevator[:floor].to_s
 			sleep 1
 		end
 	end
@@ -42,8 +66,13 @@ class Elevator
 	def randomAction
 		while true do
 			action = ["down", "up"]
-			@@person << {action: action[rand(2)], in_floor: rand(5), goes_to: nil, state: "off"}
-			sleep 7
+			res_action = action[rand(2)]
+			res_floor = rand(5)
+			id = rand(100)
+			@@person << {action: res_action, in_floor: res_floor, state: "off", id: id}
+			puts "A new call is made: the person with the ID " + id.to_s + " wants to go " + res_action + " and is in floor " + res_floor.to_s
+			self.class.sortQueue
+			sleep 3
 		end
 	end
 end
