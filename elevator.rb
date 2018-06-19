edd81adffeda04b9ca916a7d3cd334ac08dc7a1a
Charlélie Boussaud <charlelie.boussaud@hotmail.fr>
2018-06-19 15:56:58 +0200

    queue priorities are dealt with, just need to fix infinite loop

--- elevator.rb
+++ elevator.rb
@@ -5,15 +5,13 @@ class Elevator
 			
 	def self.sortQueue
 		tab_up, tab_down = @@person.partition { |x| x[:action] == "up" }
-	#	up_a, up_b = tab_up.partition { |x| x[:in_floor] > @@elevator[:floor] }
-	#	up_a.sort_by! { |k| k[:in_floor] }
-	#	tab_up = up_a.push(*up_b)
-	#	down_a, down_b = tab_down.partition { |x| x[:in_floor] > @@elevator[:floor] }
-	#	down_a.sort_by! { |k| k[:in_floor] }.reverse!
-	#	down_b.sort_by! { |k| k[:in_floor] }.reverse!
-	#   tab_down = down_b.push(*down_a)
-		tab_down.sort_by! { |k| k[:in_floor] }.reverse!
-		tab_up.sort_by! { |k| k[:in_floor] }
+		up_a, up_b = tab_up.partition { |x| x[:in_floor] > @@elevator[:floor] }
+		up_a.sort_by! { |k| k[:in_floor] }
+		tab_up = up_a.push(*up_b)
+		down_a, down_b = tab_down.partition { |x| x[:in_floor] > @@elevator[:floor] }
+		down_a.sort_by! { |k| k[:in_floor] }.reverse!
+		down_b.sort_by! { |k| k[:in_floor] }.reverse!
+		tab_down = down_b.push(*down_a)
 		if @@elevator[:status] == "up"
 			@@person = tab_up.push(*tab_down)
 		elsif @@elevator[:status] == "down"
@@ -24,10 +22,10 @@ class Elevator
 	def protoElevator
 		while true do
 			if @@person.any?
-				if @@person[0][:state] == 'off' and @@elevator[:floor] < @@person[0][:in_floor]
+				if @@elevator[:floor] < @@person[0][:in_floor]
 					@@elevator[:status] = "up"
 					@@elevator[:floor] += 1
-				elsif @@person[0][:state] == 'off' and @@elevator[:floor] > @@person[0][:in_floor]
+				elsif @@elevator[:floor] > @@person[0][:in_floor]
 					@@elevator[:status] = "down"
 					@@elevator[:floor] -= 1
 				elsif @@person[0][:state] == 'off' and @@elevator[:floor] == @@person[0][:in_floor]
@@ -46,12 +44,6 @@ class Elevator
 						end
 					end
 					puts "Person " + @@person[0][:id].to_s + " is entering the elevator and wants to go at floor " + @@person[0][:in_floor].to_s
-				elsif @@person[0][:state] == 'on' and @@elevator[:floor] < @@person[0][:in_floor]
-					@@elevator[:status] = "up"
-					@@elevator[:floor] += 1
-				elsif @@person[0][:state] == 'on' and @@elevator[:floor] > @@person[0][:in_floor]
-					@@elevator[:status] = "down"
-					@@elevator[:floor] -= 1
 				elsif @@person[0][:state] == 'on' and @@elevator[:floor] == @@person[0][:in_floor]
 					puts "Person " + @@person[0][:id].to_s + " is exiting the elevator"
 					@@person.shift
@@ -67,7 +59,11 @@ class Elevator
 		while true do
 			action = ["down", "up"]
 			res_action = action[rand(2)]
-			res_floor = rand(5)
+			if res_action == "down"
+				res_floor = rand(1...5)
+			elsif res_action == "up"
+				res_floor = rand(0...4)
+			end
 			id = rand(100)
 			@@person << {action: res_action, in_floor: res_floor, state: "off", id: id}
 			puts "A new call is made: the person with the ID " + id.to_s + " wants to go " + res_action + " and is in floor " + res_floor.to_s
